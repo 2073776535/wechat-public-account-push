8e4fd8614e0f6581eed1468d034fb30c239506a6
2073776535 <74645368+2073776535@users.noreply.github.com>
2022-09-15 18:53:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -90,10 +90,103 @@ export const config = {
       festivals: [],
       customizedDateList: []
     },
+    
+    
     // 你可以不断按格式往下增加
     // ...
   ],
+  // 公众号APP_ID
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
+  APP_ID: "wxcec7890c54e411e2",
 
+  // 公众号APP_SECRET
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
+  APP_SECRET: "e98c538896cc90fc6c3ed70db6f9fdd1",
+
+  // 是否给文字设置多彩颜色, 和emoji不兼容
+  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
+  isShowColor: true,
+
+  /**
+   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
+   */
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: "贺彤彤&王亚鹏",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "oEKqK6IONnPY5GpfE1jE8cygjx18",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "RP1ILoAAB-lPpMen34H4MkMoln2l4f3F8jUKTVG9SQA",
+      // 所在省份
+      province: "河南",
+      // 所在城市
+      city: "濮阳",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '7-7',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {"type": "生日", "name": "老婆", "year": "2022", "date": "07-07"},
+        {"type": "节日", "name": "相识纪念日", "year": "2022", "date": "07-23"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        {"keyword": "love_day", date: "2022-07-23"},
+        // 结婚纪念日
+        {"keyword": "marry_day", date: "2022-07-23"},
+        // 退伍日
+        {"keyword": "ex_day", date: "2022-07-23"},
+      ]
+    },
+    {
+      name: "贺彤彤",
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      horoscopeDate: '',
+      horoscopeDateType: '',
+      openUrl: "https://wangxinleo.cn",
+      festivals: [],
+      customizedDateList: []
+    },
+    {
+      name: "老婆2",
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      horoscopeDate: '',
+      horoscopeDateType: '',
+      openUrl: "https://wangxinleo.cn",
+      festivals: [],
+      customizedDateList: []
+    },
+    {
+      name: "老婆3",
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      horoscopeDate: '',
+      horoscopeDateType: '',
+      openUrl: "https://wangxinleo.cn",
+      festivals: [],
+      customizedDateList: []
+    },
+    
+    
+    // 你可以不断按格式往下增加
+    // ...
+  ],
   /**
    * 【推送完成提醒】 相关，主要用来展示发送是否成功/失败的数据
    */
