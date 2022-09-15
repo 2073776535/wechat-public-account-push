b2520e06deb7d596c80ae0bb8ea5726209cb7cbb
2073776535 <74645368+2073776535@users.noreply.github.com>
2022-09-15 19:09:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -21,32 +21,12 @@ export const config = {
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
   USERS: [
-    {// 想要发送的人的名字
-      name: "贺彤彤&王亚鹏",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oEKqK6IONnPY5GpfE1jE8cygjx18",
-      // 你想对他发送的模板消息的模板ID
-      useTemplateId: "RP1ILoAAB-lPpMen34H4MkMoln2l4f3F8jUKTVG9SQA",
-      // 所在省份
-      province: "河南",
-      // 所在城市
-      city: "濮阳",
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '7-7',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "",
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "生日", "name": "老婆", "year": "2022", "date": "07-07"},
-        {"type": "节日", "name": "相识纪念日", "year": "2022", "date": "07-23"},
     {
       // 想要发送的人的名字
       name: "贺彤彤&王亚鹏",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oEKqK6HNAfpiUnr8aCdEegMxpUZs",
+      id: "oEKqK6IONnPY5GpfE1jE8cygjx18",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "RP1ILoAAB-lPpMen34H4MkMoln2l4f3F8jUKTVG9SQA",
       // 所在省份
